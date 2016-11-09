144dc2cd100bd2953445d6b8c9a3bb34017e0d23
gaw1990 <georgewambold@gmail.com>
2016-11-09 16:57:32 -0600

    Modify seeds so there's no way they can overpay a loan and trigger a validation error

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
   Loan.create!(funded_amount_cents: 100_00)
 end
 
-20.times do 
-  Payment.create!(amount_cents: 10_00, loan: Loan.order("RANDOM()").first)
+15.times do 
+  Payment.create!(amount_cents: 5_00, loan: Loan.order("RANDOM()").first)
 end
