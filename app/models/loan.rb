ac52a0f1b50009dc390b99655e45223b73c8d8a9
gaw1990 <georgewambold@gmail.com>
2016-11-09 16:18:11 -0600

    Add outstanding_balance_cents to Loan#as_json

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,6 +2,10 @@ class Loan < ActiveRecord::Base
   has_many :payments
   monetize :funded_amount_cents
 
+  def as_json(options = {})
+    super.merge({'outstanding_balance_cents': outstanding_balance_cents})
+  end
+
   def outstanding_balance_cents
     funded_amount_cents - payments.sum(:amount_cents)
   end
