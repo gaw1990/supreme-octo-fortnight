43059c0ea96492bbf4b9fa51c9eafdb890dd01e2
gaw1990 <georgewambold@gmail.com>
2016-11-09 13:34:08 -0600

    Add Loan#outstanding_balance_cents method

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,8 @@
 class Loan < ActiveRecord::Base
   has_many :payments
   monetize :funded_amount_cents
+
+  def outstanding_balance_cents
+    funded_amount_cents - payments.sum(:amount_cents)
+  end
 end
