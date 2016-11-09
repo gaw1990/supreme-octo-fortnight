5991b2b8c4bb11386b9f5f501dcf5c2dbe54af1c
gaw1990 <georgewambold@gmail.com>
2016-11-09 16:57:02 -0600

    Write validation for payments that overpay the outstanding balance

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,13 @@
 class Payment < ActiveRecord::Base
+
+  validate :does_not_exceed_outstanding_balance
+
   belongs_to :loan
   monetize :amount_cents
+
+  def does_not_exceed_outstanding_balance
+    if (loan.outstanding_balance_cents - amount_cents) < 0 
+      errors.add(:amount_too_large, "Please try a payment amount that does not exceet the current outstanding balance of the loan.")
+    end 
+  end
 end
